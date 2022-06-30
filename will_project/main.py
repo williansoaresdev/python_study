8432d91e501f2e38d058291d9a6f62fc2319253f
Willian Soares <contact@williansoaresdev.com>
2022-06-30 12:29:27 -0300

    Add files via upload

---
+++ will_project/main.py
@@ -0,0 +1,11 @@
+# Projeto que embarca o will_framework
+# Willian Soares
+# contact@williansoaresdev.com
+
+
+# Importa o will_framework
+from will_framework import will
+
+
+if (__name__ == "__main__"):
+    print("Bem vindo, will_framework versao", will.will_framework_version())
---
+++ will_project/will_framework/will.py
@@ -0,0 +1,13 @@
+# Framework para encapsular diversas rotinas nativas do python
+# em classes próprias, permitindo assim fácil manutenção e evolução
+# no futuro.
+
+
+# Versão do framework controlada manualmente
+will_version = '1.0.0';
+
+
+# will_framework_version():
+# @return A versão armazenada em "will_version"
+def will_framework_version():
+    return will_version

7b8bbf2a1eebf0612d2d1f4600ead58d882d30b0
Willian Soares <contact@williansoaresdev.com>
2022-06-30 13:41:23 -0300

    Add files via upload

--- will_project/main.py
+++ will_project/main.py
@@ -7,5 +7,12 @@
 from will_framework import will
 
 
+# Instancia um objeto da classe principal do framework
+will_tools = will.WillTools()
+
+
 if (__name__ == "__main__"):
+    # Bem vindo com a versão do framework
     print("Bem vindo, will_framework versao", will.will_framework_version())
+    # Status da instancia, para checar que o objeto foi criado adequadamente
+    print("WillTools Status:", will_tools.get_status())

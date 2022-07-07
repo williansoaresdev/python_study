0aead9c099616a7eef9bf6a3d3daae9d828bddfa
Willian Soares <contact@williansoaresdev.com>
2022-07-07 17:25:26 -0300

    Versão 1.0.1
    Iniciando a classe de log e de utilidades

--- will_project/will_framework/will.py
+++ will_project/will_framework/will.py
@@ -3,8 +3,11 @@
 # no futuro.
 
 
+import datetime
+
+
 # Versão do framework controlada manualmente
-will_version = '1.0.0';
+will_version = '1.0.1';
 
 
 # alguns status de controle interno
@@ -17,6 +20,70 @@ def will_framework_version():
     return will_version
 
 
+# classe para gerar logs das operacoes da aplicação
+# como por exemplo o log por print("")
+class WillLog:
+    def __init__(self, owner):
+        # indica se o log está ativo
+        # se inativo, não irá jogar nenhum log no command
+        self.__log_active = True
+        # objeto pai (WillTools)
+        self.__owner = owner
+
+
+    # dt_log():
+    # Imprime um texto no console desde que o log esteja ATIVO (__log_active)
+    # Antecipado pela data hora do registro
+    # @text: texto a ser impresso no console
+    def dt_log(self, text):
+        if self.__log_active:
+            date = self.__owner.utils.date()
+            hour = self.__owner.utils.full_hour()
+            print("{} {} - {}".format(date, hour, text))
+
+
+    # log():
+    # Imprime um texto no console desde que o log esteja ATIVO (__log_active)
+    # @text: texto a ser impresso no console
+    def log(self, text):
+        if self.__log_active:
+            print(text)
+
+
+    # text():
+    # Imprime qualquer texto no console
+    # Dica: este comando não respeita o __log_active, para isso, use o log()
+    # @text: texto a ser impresso no console
+    def text(self, text):
+        print(text)
+
+
+    pass
+
+
+# classe WillUtils:
+# agrega comandos simples que não precisam estar em
+# uma classe própria
+# se por acaso um conjunto de comandos começar
+# a crescer demais, então ele pode ser encapsulado
+# em uma classe
+class WillUtils:
+    # WillUtils.full_hour():
+    # retorna a hora no formato hh:mm:ss
+    def full_hour(self):
+        now = datetime.datetime.now()
+        return now.strftime("%H:%M:%S")
+
+
+    # WillUtils.date():
+    # retorna a data no formato dd/mm/yy
+    def date(self):
+        now = datetime.datetime.now()
+        return now.strftime("%d/%m/%y")
+
+    pass
+
+
 # classe principal do framework
 # agrega as demais classes e funcionalidades em
 # uma única instância geral de controle
@@ -25,6 +92,10 @@ class WillTools:
         # status de controle interno do objeto
         # inicia como "Pronto"
         self.__status = will_status_ready
+        # ferramente de log do framework
+        self.logs = WillLog(self)
+        # utilidades gerais
+        self.utils = WillUtils()
 
 
     # will_tools.get_status():

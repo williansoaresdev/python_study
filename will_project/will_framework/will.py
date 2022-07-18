463c23bbfbc71b4c5b0b71351a924383028b8394
Willian Soares <contact@williansoaresdev.com>
2022-07-18 09:53:56 -0300

    Log com método para limpar a tela

--- will_project/will_framework/will.py
+++ will_project/will_framework/will.py
@@ -3,11 +3,11 @@
 # no futuro.
 
 
-import datetime
+import datetime, os
 
 
 # Versão do framework controlada manualmente
-will_version = '1.0.1';
+will_version = '1.0.2';
 
 
 # alguns status de controle interno
@@ -24,11 +24,41 @@ def will_framework_version():
 # como por exemplo o log por print("")
 class WillLog:
     def __init__(self, owner):
+        # indica se deve gravar o log no disco
+        self.__log_on_disk = False
+        # caminho para salvar o log
+        self.__log_path = 'C:\pythonLog'
+        # indica se deve logar no console as informações
+        self.__log_on_console = True
         # indica se o log está ativo
         # se inativo, não irá jogar nenhum log no command
         self.__log_active = True
         # objeto pai (WillTools)
         self.__owner = owner
+
+
+    # clearScreen():
+    # Limpa o console
+    def clearScreen(self):
+        os.system('cls||clear')
+
+
+    # disable_log():
+    # Desativa o log alterando o atributo __log_active
+    def disable_log(self):
+        self.__log_active = False
+
+
+    # disable_log_ondisk():
+    # Desativa a gravação do log em disco
+    def disable_log_ondisk(self):
+        self.__log_on_disk = False
+
+
+    # disable_log_on_console():
+    # Desativa o log no console (prompt)
+    def disable_log_on_console(self):
+        self.__log_on_console = False
 
 
     # dt_log():
@@ -39,7 +69,25 @@ class WillLog:
         if self.__log_active:
             date = self.__owner.utils.date()
             hour = self.__owner.utils.full_hour()
-            print("{} {} - {}".format(date, hour, text))
+            self.log("{} {} - {}".format(date, hour, text))
+
+
+    # enable_log():
+    # Ativa o log alterando o atributo __log_active
+    def enable_log(self):
+        self.__log_active = True
+
+
+    # enable_log_on_console():
+    # Ativa o log no console (prompt)
+    def enable_log_on_console(self):
+        self.__log_on_console = False
+
+
+    # enable_log_ondisk():
+    # Ativa a gravação do log em disco
+    def enable_log_ondisk(self):
+        self.__log_on_disk = True
 
 
     # log():
@@ -47,16 +95,28 @@ class WillLog:
     # @text: texto a ser impresso no console
     def log(self, text):
         if self.__log_active:
-            print(text)
+            if (self.__log_on_console):
+                print(text)
 
 
     # text():
     # Imprime qualquer texto no console
-    # Dica: este comando não respeita o __log_active, para isso, use o log()
+    # Dica: este comando não respeita o __log_active e nem o __log_on_console,
+    # para isso, use o log() ou dt_log(), ou seja,
+    # este text() atua como um print() comum
     # @text: texto a ser impresso no console
     def text(self, text):
         print(text)
 
+
+    pass
+
+
+# classe WillStringList
+# permite manipular arquivos de texto
+# criando e lendo arquivos de texto de várias linhas
+# Detalhe: esta classe deve ser usada independente de WillTools
+class WillStringList:
 
     pass
 
